1ae64d6990fffb569b48c253b76ef3d1ad6633c8
Pham Hong Thai <hongthaipro@gmail.com>
2021-11-26 19:56:45 +0700

    ⚔️ Setup data for character NFTs

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,16 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["bulbasaur", "charmander", "squirtle", "caterpie"], // Names
+    [
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png", // Images
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png",
+    ],
+    [100, 200, 300, 150], // HP values
+    [100, 50, 25, 70] // Attack damage values
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
